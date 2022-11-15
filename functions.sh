de197689faeed9037b6016b166937dc32af360ef
Sandeep <sandeep@opstree.com>
2022-11-15 12:17:52 +0530

    Added generic function of saveTaskStatus

--- functions.sh
+++ functions.sh
@@ -24,18 +24,18 @@ function saveTaskStatus() {
   TASK_STATUS=$1
   ACTIVITY_SUB_TASK_CODE=$2  
 
-  if [ $TASK_STATUS -eq 0 ]
+  if [ "$TASK_STATUS" -eq 0 ]
   then
     logInfoMessage "Congratulations ${ACTIVITY_SUB_TASK_CODE} succeeded!!!"
-    generateOutput mvn_execute true "Congratulations ${ACTIVITY_SUB_TASK_CODE} succeeded!!!"
-  elif [ $VALIDATION_FAILURE_ACTION == "FAILURE" ]
+    generateOutput "${ACTIVITY_SUB_TASK_CODE}" true "Congratulations ${ACTIVITY_SUB_TASK_CODE} succeeded!!!"
+  elif [ "$VALIDATION_FAILURE_ACTION" == "FAILURE" ]
     then
-      logErrorMessage "Please check ${ACTIVITY_SUB_TASK_CODE failed!!!"
-      generateOutput ${ACTIVITY_SUB_TASK_CODE} false "Please check ${ACTIVITY_SUB_TASK_CODE failed!!!"
+      logErrorMessage "Please check ${ACTIVITY_SUB_TASK_CODE} failed!!!"
+      generateOutput "${ACTIVITY_SUB_TASK_CODE}" false "Please check ${ACTIVITY_SUB_TASK_CODE} failed!!!"
       exit 1
     else
       logWarningMessage "Please check ${ACTIVITY_SUB_TASK_CODE} failed!!!"
-      generateOutput ${ACTIVITY_SUB_TASK_CODE} true "Please check ${ACTIVITY_SUB_TASK_CODE} failed!!!"
+      generateOutput "${ACTIVITY_SUB_TASK_CODE}" true "Please check ${ACTIVITY_SUB_TASK_CODE} failed!!!"
   fi
 }
 
