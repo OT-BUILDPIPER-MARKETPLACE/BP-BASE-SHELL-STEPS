5cdc118384489661c17a2123c4cf34f1b301b6f1
Priyanshi Chauhan <107524071+Cpriyanshi77@users.noreply.github.com>
2023-07-13 18:13:23 +0530

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -6,7 +6,7 @@ generateOutput() {
   Message=$3
   EXECUTION_DIR="/bp/execution_dir"
   OUTPUT_DIR=${EXECUTION_DIR}/${EXECUTION_TASK_ID}
-  sudo mkdir -p "${OUTPUT_DIR}"
+  mkdir -p "${OUTPUT_DIR}"
   echo "{ \"${Task}\": {\"status\": \"${Status}\", \"message\": \"${Message}\"}}"  | jq . > "${OUTPUT_DIR}"/summary.json
   echo "{ \"status\": \"${Status}\", \"message\": \"${Message}\"}"  | jq . > "${OUTPUT_DIR}"/"${Task}".json
 }
