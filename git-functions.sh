a7854ef376b4b24171b21af959b2515d3f199e7a
Sandeep <sandeep@opstree.com>
2023-10-18 10:24:04 +0530

    Added function to get committer of files in a certain branch

--- git-functions.sh
+++ git-functions.sh
@@ -15,12 +15,31 @@ function findConflictingFiles() {
     git checkout -q "${TGT_BRANCH}"
 #    git pull origin ${TGT_BRANCH}
 
-    git checkout -q temp_merge_branch
-    git merge -q "$SRC_BRANCH"
+    git checkout -q -b temp_merge_branch
+    git merge -q "$SRC_BRANCH" 1> /dev/null
 
     conflicts=$(git diff --name-only --diff-filter=U)
-    git merge -q --abort
+    git merge --abort
     git checkout -q "${TGT_BRANCH}"
-    git branch -D temp_merge_branch
+    git branch -q -D temp_merge_branch
     echo $conflicts
+}
+
+function getLastAuthorOfFile() {
+    BRANCH=$1
+    FILE=$2
+
+    git checkout -q $BRANCH
+
+    git log -1  --pretty=format:"%an" ${FILE}
+}
+
+function getLastAuthorOfFiles() {
+    BRANCH=$1
+    FILES="$2"
+    for FILE in ${FILES}
+    do
+        author=`getLastAuthorOfFile ${BRANCH} ${FILE}`
+        echo "${FILE}: ${author}"
+    done
 }
