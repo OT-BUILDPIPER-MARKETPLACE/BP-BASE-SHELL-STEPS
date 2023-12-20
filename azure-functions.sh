58e1eca3d6fd2fdfa5e638b9f24882f065840936
Ashwani Singh <ashwani.singh@opstree.com>
2023-12-20 23:30:50 +0530

    add subscription

--- azure-functions.sh
+++ azure-functions.sh
@@ -4,7 +4,9 @@ function getAzureServicePrinciple() {
     ARM_CLIENT_ID=$1
     ARM_CLIENT_SECRET=$2
     ARM_TENANT_ID=$3
+    ARM_SUBSCRIPTION_ID=$4
     export ARM_CLIENT_ID="$ARM_CLIENT_ID"
     export ARM_CLIENT_SECRET="$ARM_CLIENT_SECRET"
     export ARM_TENANT_ID="$ARM_TENANT_ID"
+    export ARM_SUBSCRIPTION_ID="$ARM_SUBSCRIPTION_ID"
 }
