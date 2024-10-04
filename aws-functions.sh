a7b8d8f0630e7dcfc33e8efbb4a16261da471438
Priyanshi Chauhan <priyanshi.chauhan@opstree.com>
2024-10-04 18:05:55 +0530

    added aws functions

--- aws-functions.sh
+++ aws-functions.sh
@@ -70,9 +70,9 @@ function getAssumeRole() {
 
     export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" $creds)
 
-    echo "AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY_ID"
-    echo "AWS_SECRET_ACCESS_KEY: $AWS_SECRET_ACCESS_KEY"
-    echo "AWS_SESSION_TOKEN: $AWS_SESSION_TOKEN"
+    export "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID"
+    export "AWS_SECRET_ACCESS_KEY=AWS_SECRET_ACCESS_KEY"
+    export "AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN"
 }
 
 function set_aws_credentials() {
