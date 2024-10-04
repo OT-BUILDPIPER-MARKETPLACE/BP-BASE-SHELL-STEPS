aee055e0ff1dbe622593c278b612cea4b6117ae8
Priyanshi Chauhan <priyanshi.chauhan@opstree.com>
2024-10-05 00:22:06 +0530

    updated code

--- functions.sh
+++ functions.sh
@@ -265,20 +265,4 @@ function getServiceName() {
   getNthTextInALine "$PROJECT_SVC_NAME" : 1
 }
 
-function jsonOutput() {
-    file_name="$1"
-    output_vars="$2"
-
-    file_content=""
-    if [[ -f "$file_name" ]]; then
-        file_content=$(<"$file_name")
-    fi
-    [[ "$file_content" != "["* ]] && file_content="[]"
-
-    updated_content=$(jq -c ". += [$output_vars]" <<< "$file_content")
-
-    echo "$updated_content" | jq "." > "$file_name"
-
-    echo "Job step response updated in: $file_name"
-}
 
